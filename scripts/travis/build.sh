95069f056ffe9c2cdc223c23f4acdbf7d8fc64ef
Fangjun Kuang <csukuangfj@gmail.com>
2021-05-13 19:08:29 +0800

    add travis.

---
+++ scripts/travis/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+cur_dir=$(cd $(dirname $BASH_SOURCE) && pwd)
+k2_dir=$(cd $cur_dir/.. && pwd)
+
+echo "=============================="
+echo " Display python version       "
+echo "------------------------------"
+
+python3 --version
+which python3
+
+
+# $k2_dir/
+
