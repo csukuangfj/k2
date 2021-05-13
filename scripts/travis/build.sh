f45c68e27d7483ed5e18708ecee25fb9cfa297c3
Fangjun Kuang <csukuangfj@gmail.com>
2021-05-13 19:33:48 +0800

    minor fixes

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -15,18 +15,15 @@ which pyenv
 pyenv install --list
 pushd /opt/pyenv
 git branch
-git remote -v
+git checkout master
+git pull
+git log -3
+popd
+pyenv install --list
+#
+pyenv install $K2_PYTHON_VERSION_LONG
+python3 --version
 which python3
-# git pull
-# git merge --ff orgin/master
-# git log -3 --format=oneline
-# popd
-# pyenv install --list
-#
-# pyenv install 3.8.0
-#
-# python3 --version
-# which python3
 
 
 echo "=============================="
